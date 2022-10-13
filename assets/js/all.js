621fe9e2ffc275150ad0ceff3bf1d6aa4317c377
Clara <smile629639@gmail.com>
2022-10-14 02:01:56 +0800

    Update 2022-10-13T18:01:53.309Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,23 +1,22 @@
+"use strict";
+
 $(function () {
   $('.problem-box1').click(function () {
     $('.problem-content1').toggle();
     $('.problem-title1').toggleClass('active');
     $('img', this).toggle();
-  })
-
+  });
   $('.problem-box2').click(function () {
     $('.problem-content2').toggle();
     $('.problem-title2').toggleClass('active');
     $('img', this).toggle();
-  })
-
+  });
   $('.problem-box3').click(function () {
     $('.problem-content3').toggle();
     $('.problem-title3').toggleClass('active');
     $('img', this).toggle();
-  })
+  });
 });
-
 
 function gototop() {
   window.scrollTo({
@@ -25,3 +24,4 @@ function gototop() {
     behavior: 'smooth'
   });
 }
+//# sourceMappingURL=all.js.map
