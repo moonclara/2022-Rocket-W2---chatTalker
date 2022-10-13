045a3b37c8a03b0820b8ea0709b73a680d9e5c5a
Clara <smile629639@gmail.com>
2022-10-14 02:01:04 +0800

    first commit\

---
+++ app/assets/js/all.js
@@ -0,0 +1,27 @@
+$(function () {
+  $('.problem-box1').click(function () {
+    $('.problem-content1').toggle();
+    $('.problem-title1').toggleClass('active');
+    $('img', this).toggle();
+  })
+
+  $('.problem-box2').click(function () {
+    $('.problem-content2').toggle();
+    $('.problem-title2').toggleClass('active');
+    $('img', this).toggle();
+  })
+
+  $('.problem-box3').click(function () {
+    $('.problem-content3').toggle();
+    $('.problem-title3').toggleClass('active');
+    $('img', this).toggle();
+  })
+});
+
+
+function gototop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
